65c4fd65f8db6cc54e1af0c5f3cc354e444a7594
Sergii Anikin <sergeant.ua@gmail.com>
2021-06-04 03:57:19 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  const [ type, setType ] = useState(null);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="btn-wrapper">
+        <button onClick={() => setType('class')}>ClassBased</button>
+        <button onClick={() => setType('func')}>Functional</button>
+      </div>
+      {type && <div>{type}</div>}
     </div>
   );
 }
